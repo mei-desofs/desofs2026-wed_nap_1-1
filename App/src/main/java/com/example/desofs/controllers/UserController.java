eb0fe4656c1312a396c3d171fb772a3bca0fac8e
pedro <1221033@isep.ipp.pt>
2026-05-13 23:48:11 +0100

    feat: UserController.java small changes

--- App/src/main/java/com/example/desofs/controllers/UserController.java
+++ App/src/main/java/com/example/desofs/controllers/UserController.java
@@ -30,7 +30,6 @@ public class UserController {
         roleGuard.requireRole(jwt, Role.ADMIN);
         return ResponseEntity.ok(userService.getAllUsers());
     }
-    
 
     @PostMapping("/{id}/roles")
     public ResponseEntity<Void> assignRole(
@@ -41,7 +40,6 @@ public class UserController {
         userService.assignRole(jwt.getSubject(), id, dto.role());
         return ResponseEntity.noContent().build();
     }
-
 
     @DeleteMapping("/{id}/roles")
     public ResponseEntity<Void> removeRole(
